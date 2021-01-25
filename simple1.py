8fa455d1d7e3813e813c977023884055a177bd1a
Yuka <yuka.couweleers@gmail.com>
2021-01-24 19:30:36 -0500

    Add new file

---
+++ simple1.py
@@ -0,0 +1,16 @@
+'''
+A Very Simple Script to practice using pylint to test our code. This scored 10 out of 10!
+'''
+
+def myfunc():
+
+    '''
+    A simple function
+    '''
+
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
